cc967bb31361fa0cdac233b95ab948b443c58c50
Renan Lessa <renanlessa@gmail.com>
2020-07-28 15:38:51 -0300

    setup inicial

---
+++ src/components/Menu/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import Logo from './../../assets/logo.png';
+import './Menu.css';
+import ButtonLink from './components/ButtonLink';
+
+function Menu() {
+    return (
+        <nav className="Menu">
+            <a href="/">
+                <img className="Logo" src={Logo} alt="LessaFlix"/>
+            </a>
+
+            <ButtonLink className="ButtonLink" href="/">
+                Novo vídeo
+            </ButtonLink>
+        </nav>
+    );
+}
+
+export default Menu;
